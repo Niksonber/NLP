23baad7ed7e234d274a1378dd3e513e47aade7f9
niksonber <nik.son18@hotmail.com>
2020-08-22 14:08:16 -0300

    adição de classe para manipular dados

---
+++ dataHandler.py
@@ -0,0 +1,61 @@
+# based on https://github.com/pytorch/tutorials/blob/master/intermediate_source/seq2seq_translation_tutorial.py
+# adapted by nikson
+
+import csv
+import unicodedata
+import re
+from boW import BoW
+from nltk.stem.snowball import SnowballStemmer
+
+class DataHandler:
+    def __init__(self):
+        self.askDictionary = BoW('ask')
+        self.ansDictionary= BoW('answers')
+        self._stopWords = self.readStopWords()
+        self._porter = SnowballStemmer("portuguese")
+    
+    def readStopWords(self):
+        with open('stopWords', 'rt', encoding='utf-8') as f:
+            words = f.read().split('\n')
+        return words[:-1]
+
+    # Turn a Unicode string to plain ASCII, thanks to
+    # https://stackoverflow.com/a/518232/2809427
+    def unicodeToAscii(self,s):
+        return ''.join(
+            c for c in unicodedata.normalize('NFD', s)
+            if unicodedata.category(c) != 'Mn'
+        )
+
+    # Lowercase, trim, and remove non-letter characters
+    def normalize(self, s):
+        s = self.unicodeToAscii(s.lower().strip())
+        s = re.sub(r"([.!?])", r" \1", s)
+        s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+        return s
+
+    def preProcess(self, s):
+        s = self.normalize(s)
+        words = s.strip().split(' ')
+        #remove stopwords
+        words = [w for w in words if not w in self._stopWords]
+        #stem 
+        stemmed = [self._porter.stem(word) for word in words[:100]]
+        return stemmed
+
+    def readData(self):
+        with open('dataset.csv', 'rt', encoding='utf-8') as f:
+            data = list(csv.reader(f))
+        return data
+
+    def createDictionary(self):
+        data = self.readData()
+        for (ask,ans,emo) in data:
+            self.ansDictionary.addSentence(self.normalize(ans + " " + ask))
+            self.askDictionary.addSentence(self.preProcess(ans + " " + ask))
+        print("Ask dictionary length: {}".format(self.askDictionary.n_words))
+        print("Answers dictionary length: {}".format(self.ansDictionary.n_words))
+
+if __name__== '__main__':
+    d = DataHandler()
+    d.createDictionary()
